dd5e6781ae6dd02d329e2d9496b28efe8bae1526
StaThin <giovanni.m.marchetti@gmail.com>
2019-04-05 13:31:09 +0200

    move folder data

--- data/aspirin.R
+++
@@ -1,4 +0,0 @@
-"aspirin" <-
-structure(list(y = c(189, 104), ny = c(10845, 10933), a = structure(c(1, 
-2), .Label = c("1", "2"), class = "factor")), .Names = c("y", 
-"ny", "a"), row.names = c("1", "2"), class = "data.frame")
--- data/radelet2.R
+++
@@ -1,4 +0,0 @@
-"radelet2" <-
-structure(c(19, 17, 141, 149), .Dim = c(2, 2), .Dimnames = structure(list(
-    drace = c("white", "black"), death = c("y", "n")), .Names = c("drace", 
-"death")))
